e815a15eb7d8ab3d950e178e782e8d6adc513471
Manuel <manuel.hofer@icg.tugraz.at>
2015-09-02 10:49:18 +0200

    removed references to Eigen from CUDA files

--- cudawrapper.h
+++ cudawrapper.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "helper_math.h"
 
 // internal
-#include "commons.h"
 #include "sparsematrix.h"
 #include "dataArray.h"
 
